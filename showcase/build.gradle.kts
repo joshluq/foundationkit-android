cea98e80d40e402046c1e1ddde914b3be32747db
Scaffolder <scaffolder@backstage.io>
2026-01-17 14:30:12 +0000

    Initial commit from Templatekit [skip ci]

---
+++ showcase/build.gradle.kts
@@ -0,0 +1,23 @@
+import com.android.build.api.dsl.ApplicationExtension
+
+plugins {
+    alias(libs.plugins.pluginkit.android.application)
+    alias(libs.plugins.pluginkit.android.compose)
+    alias(libs.plugins.pluginkit.android.hilt)
+    alias(libs.plugins.pluginkit.quality)
+    alias(libs.plugins.pluginkit.android.testing)
+}
+
+configure<ApplicationExtension> {
+    namespace = "es.joshluq.foundationkit.showcase"
+
+    defaultConfig {
+        applicationId = "es.joshluq.foundationkit.showcase"
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation(project(":foundationkit"))
+}
