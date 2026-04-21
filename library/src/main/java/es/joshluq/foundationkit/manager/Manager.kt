f35508c10e8cf9e686e186940bdf50aa1f6c2730
Josh Luque <josh.luque@gmail.com>
2026-04-21 17:23:15 +0200

    Refactor `Manager` class to remove initialization state logic and introduce `ManagerBuilder` interface

--- library/src/main/java/es/joshluq/foundationkit/manager/Manager.kt
+++ library/src/main/java/es/joshluq/foundationkit/manager/Manager.kt
@@ -2,20 +2,12 @@ package es.joshluq.foundationkit.manager
 
 abstract class Manager<T : ManagerConfig> {
 
-    protected var isInitialized = false
     protected lateinit var config: T
 
-    open fun initialize(config: T): Result<Unit> = runCatching {
-        this.config = config
-        isInitialized = true
-        onInitialize()
-    }
+}
 
-    protected open fun onInitialize() { }
+interface ManagerBuilder<T : ManagerConfig> {
 
-    protected fun checkInitialization() {
-        check(isInitialized) {
-            "Manager ${this::class.simpleName} is not initialized. Call initialize() first."
-        }
-    }
+    fun build(config: T): Manager<T>
+
 }
