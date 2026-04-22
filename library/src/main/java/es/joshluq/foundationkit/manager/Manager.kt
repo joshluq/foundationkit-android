41d27152e2de9b4d439fea904d85e77a736cc5ea
Josh Luque <josh.luque@gmail.com>
2026-04-22 22:44:31 +0200

    Add `@Volatile` annotation to `config` and implement `isConfigInitialized` check in `Manager` class

--- library/src/main/java/es/joshluq/foundationkit/manager/Manager.kt
+++ library/src/main/java/es/joshluq/foundationkit/manager/Manager.kt
@@ -2,7 +2,13 @@ package es.joshluq.foundationkit.manager
 
 abstract class Manager<T : ManagerConfig> {
 
+    @Volatile
     protected lateinit var config: T
+
+    /**
+     * Internal check to ensure config is initialized before use.
+     */
+    protected fun isConfigInitialized(): Boolean = ::config.isInitialized
 
 }
 
